cfe0d2eebdc7d3e273b8263ae2122297c089912b
Yelyzaveta Potekhina <91630215+DmitriyGovosyants@users.noreply.github.com>
2022-04-13 13:31:43 +0300

    fixed - minor editing in js files

---
+++ src/js/back-to-top-btn.js
@@ -0,0 +1,16 @@
+$(function () {
+        $('.scrollup').click(function() {
+            $("html, body").animate({
+                scrollTop: 0
+            }, 500);
+        })
+    })
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 300) {
+        $('.scrollup').fadeIn();
+    }
+    else {
+        $('.scrollup').fadeOut();
+    }
+});
