bbac9d822d838172771d0e98d58e279ffa98dff5
Yelyzaveta Potekhina <91630215+DmitriyGovosyants@users.noreply.github.com>
2022-04-13 14:49:35 +0300

    fixed - clean code in read-more-hero

--- src/js/read-more-hero.js
+++ src/js/read-more-hero.js
@@ -1,8 +1,10 @@
-window.readMore = function () {
-    const points = document.getElementById("points");
-    const more = document.getElementById("next");
-    const btnText = document.getElementById("moreBtn");
+const points = document.getElementById("points");
+const more = document.getElementById("next");
+const btnText = document.getElementById("moreBtn");
 
+btnText.addEventListener('click', toggleMoreText);
+
+function toggleMoreText() {
     if (points.style.display === "none") {
         points.style.display = "inline";
         more.style.display = "none";
