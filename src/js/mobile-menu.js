dcbbad4a3fa42b9ecd76d754e2c2bb1176e60c66
Yelyzaveta Potekhina <91630215+DmitriyGovosyants@users.noreply.github.com>
2022-04-13 12:29:59 +0300

    fixed - mobile menu code optimized

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,36 +1,30 @@
-(() => {
-  const mobileMenu = document.querySelector("[mobile-menu]");
-  const openMenuBtn = document.querySelector("[open-menu]");
-  const closeMenuBtn = document.querySelector("[close-menu]");
-  const closeLinkFirst = document.querySelector("[close-link-first]");
-  const closeLinkSecond = document.querySelector("[close-link-second]");
-  const closeLinkThird = document.querySelector("[close-link-third]");
-  const closeLinkFourth = document.querySelector("[close-link-fourth]");
-  const closeLinkFifth = document.querySelector("[close-link-fifth]");
+const mobileMenu = document.querySelector("[mobile-menu]");
+const openMenuBtn = document.querySelector("[open-menu]");
+const closeMenuBtn = document.querySelector("[close-menu]");
+const mobileMenuNav = document.querySelector(".mobile-menu__list");
 
-  const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
-    document.body.classList.toggle ('overflow--is-hidden')
-  };
+openMenuBtn.addEventListener('click', toggleMenu);
+closeMenuBtn.addEventListener('click', toggleMenu);
+mobileMenuNav.addEventListener('click', onClickAnchorMobileMenuClose);
+window.matchMedia('(min-width: 1280px)').addEventListener('change', onTabletMQMobileMenuClose);
 
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
-  closeLinkFirst.addEventListener('click', toggleMenu);
-  closeLinkSecond.addEventListener('click', toggleMenu);
-  closeLinkThird.addEventListener('click', toggleMenu);
-  closeLinkFourth.addEventListener('click', toggleMenu);
-  closeLinkFifth.addEventListener('click', toggleMenu);
+function toggleMenu() {
+  const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+  openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+  mobileMenu.classList.toggle('is-open');
+  document.body.classList.toggle('overflow--is-hidden');
+};
 
-  // Закрываем мобильное меню на более широких экранах
-  // в случае изменения ориентации устройства.
-  window.matchMedia('(min-width: 1280px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
-  });
-})();
+function onClickAnchorMobileMenuClose(e) {
+  if (e.target.nodeName !== 'A') return;  
+  toggleMenu();
+};
+
+function onTabletMQMobileMenuClose(e) {
+  if (!e.matches) return;
+  mobileMenu.classList.remove('is-open');
+  openMenuBtn.setAttribute('aria-expanded', false);
+  document.body.classList.remove('overflow--is-hidden');
+};
+
 
--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,14 +1,11 @@
 const anchors = document.querySelectorAll('a[href*="#"]')
 
 for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault()
+  anchor.addEventListener('click', e => {
+    e.preventDefault();
     
-    const blockID = anchor.getAttribute('href').substr(1)
+    const blockID = anchor.getAttribute('href').substring(1);
     
-    document.getElementById(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
-  })
+    document.getElementById(blockID).scrollIntoView({ behavior: 'smooth', });
+  });
 }
